68215db6e9eeb5d7beca46b39e18c29c4a4b78f5
lihaoliang <cvbmgh@126.com>
2017-08-24 22:32:20 +0800

    no message

--- controllers/TimedTaskController.php
+++ controllers/TimedTaskController.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ *
+ * 定时任务测试
  * Created by PhpStorm.
  * User: DELL
  * Date: 2017/8/24
@@ -7,9 +9,14 @@
  */
 
 namespace app\controllers;
+use yii\web\Controller;
 
 
-class TimedTaskController
+class TimedTaskController extends Controller
 {
-
+    public function actionIndex(){
+        echo '执行';
+        file_put_contents('D:\Zdtvr\Projects\Test\TestPrj1\timed.txt',"\n项目根目录timed.txt文件,当前时间:".date('Y-m-d H:i:s',time()),FILE_APPEND);
+        file_put_contents('timed.txt',"\nweb/timed.txt文件,当前时间:".date('Y-m-d H:i:s',time()),FILE_APPEND);
+    }
 }
